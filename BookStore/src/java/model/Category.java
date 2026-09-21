9d7c553c475f48287b185a0c20c4dbb673fa05f2
Trịnh Huy Hoàng <trinhhuyhoang04122005@gmail.com>
2026-09-21 22:56:08 +0700

    Bo sung constructor khong thuoc tinh cartId

--- BookStore/src/java/model/Category.java
+++ BookStore/src/java/model/Category.java
@@ -14,6 +14,11 @@ public class Category {
         this.description = description;
     }
 
+    public Category(String categoryName, String description) {
+        this.categoryName = categoryName;
+        this.description = description;
+    }
+
     public int getCategoryId() {
         return categoryId;
     }
