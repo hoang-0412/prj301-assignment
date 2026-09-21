e6d3ecf81568dc40b6529791b1f1c72123677d77
Trịnh Huy Hoàng <trinhhuyhoang04122005@gmail.com>
2026-09-21 20:57:41 +0700

    insertBook

--- BookStore/src/java/dal/BookDAO.java
+++ BookStore/src/java/dal/BookDAO.java
@@ -63,6 +63,40 @@ public class BookDAO extends DBContext {
         return null;
     }
 
+    public int insertBook(Book b) {
+        String sql = "INSERT INTO [dbo].[Book]\n"
+                + "           ([categoryId]\n"
+                + "           ,[title]\n"
+                + "           ,[author]\n"
+                + "           ,[publisher]\n"
+                + "           ,[publicationYear]\n"
+                + "           ,[isbn]\n"
+                + "           ,[price]\n"
+                + "           ,[stockQuantity]\n"
+                + "           ,[description]\n"
+                + "           ,[createdAt])\n"
+                + "     VALUES\n"
+                + "           (?,?,?,?,?,?,?,?,?,?)";
+        int n = 0;
+        try {
+            PreparedStatement ptm = connection.prepareStatement(sql);
+            ptm.setInt(1, b.getCategoryId());
+            ptm.setString(2, b.getTitle());
+            ptm.setString(3, b.getAuthor());
+            ptm.setString(4, b.getPublisher());
+            ptm.setInt(5, b.getPublicationYear());
+            ptm.setString(6, b.getIsbn());
+            ptm.setDouble(7, b.getPrice());
+            ptm.setInt(8, b.getStockQuantity());
+            ptm.setString(9, b.getDescription());
+            ptm.setDate(10, b.getCreatedAt());
+            n = ptm.executeUpdate();
+        } catch (SQLException ex) {
+            ex.getStackTrace();
+        }
+        return n;
+    }
+
     public static void main(String[] args) {
         BookDAO dao = new BookDAO();
         Vector<Book> vector = dao.getAllBook();
